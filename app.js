0ea87483a7fc7d86e5d81cec647aca36c65e9c8d
LiuYao <670652018@qq.com>
2017-03-22 00:19:06 +0800

    获取access_token

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const Koa = require('koa');
 const wechat = require('./wechat/async');
+const path = require('path');
+const wechat_file = path.join(__dirname, './config/wechat.txt');
+const util = require('./libs/util');
+
 const config = {
     wechat: {
         AppID: 'wxfbd5cd64b3fc8e4d',
         AppSecret: 'a88097544e4faa1c087fda2fefcec929',
-        Token: 'full_stack_engineer'
+        Token: 'full_stack_engineer',
+        getAccessToken: function() {
+            return util.readFileAsync(wechat_file, 'utf8');
+        },
+        saveAccessToken: function(data) {
+            return util.writeFileAsync(wechat_file, JSON.stringify(data));
+        }
     }
 };
 
---
+++ libs/util.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+exports.readFileAsync = function(fpath, encoding) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(fpath, encoding, (err, content) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(content);
+            }
+        });
+    });
+};
+
+exports.writeFileAsync = function(fpath, content) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fpath, content, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
